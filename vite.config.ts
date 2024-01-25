90090964491b6a273f9029725cca5c7e07b894e3
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-01-25 16:27:18 +0100

    fix: actually working library (#17)

---
+++ vite.config.ts
@@ -0,0 +1,62 @@
+import * as path from "path";
+import typescript2 from "rollup-plugin-typescript2";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    cssInjectedByJsPlugin(),
+    typescript2({
+      check: false,
+      include: ["src/components/**/*.vue"],
+      tsconfigOverride: {
+        compilerOptions: {
+          outDir: "dist",
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+      },
+      exclude: ["vite.config.ts"],
+    }),
+  ],
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: "src/main.ts",
+      name: "reevFrontendLib",
+      formats: ["es", "cjs", "umd"],
+      fileName: (format) => `reev-frontend-lib.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that should not be bundled
+      // into your library
+      input: {
+        main: path.resolve(__dirname, "src/main.ts"),
+      },
+      external: ["vue"],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "main.css") return "reev-frontend-lib.css";
+          return assetInfo.name;
+        },
+        exports: "named",
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
