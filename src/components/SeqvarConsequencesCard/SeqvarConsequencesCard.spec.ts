e2c46dcb5e2f23418f37e56c825eef671c27b24a
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-01-30 14:57:43 +0100

    feat: adding SeqvarConsequencesCard

---
+++ src/components/SeqvarConsequencesCard/SeqvarConsequencesCard.spec.ts
@@ -0,0 +1,45 @@
+import fs from 'fs'
+import path from 'path'
+import { describe, expect, it } from 'vitest'
+
+import { SeqvarResult } from '../../api/mehari/types'
+import { setupMountedComponents } from '../../lib/testUtils'
+import SeqvarConsequencesCard from './SeqvarConsequencesCard.vue'
+
+/** Fixtures */
+const seqvarCsqResult = SeqvarResult.fromJson(
+  JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, '../../api/mehari/fixture.seqvarCsqResponse.BRCA1.json'),
+      'utf-8'
+    )
+  )
+)
+
+describe.concurrent('SeqvarConsequencesCard.vue', async () => {
+  it('renders the consequence info', async () => {
+    // arrange:
+    const { wrapper } = await setupMountedComponents(
+      { component: SeqvarConsequencesCard },
+      {
+        props: {
+          consequences: seqvarCsqResult.result
+        }
+      }
+    )
+
+    // act: nothing, only test rendering
+
+    // assert:
+    const table = wrapper.find('table')
+    expect(table.exists()).toBe(true)
+    const headers = table.findAll('th')
+    expect(headers.length).toBe(6)
+    expect(headers[0].text()).toBe('Gene')
+    expect(headers[1].text()).toBe('Transcript')
+    expect(headers[2].text()).toBe('Consequence')
+    expect(headers[3].text()).toBe('HGVS.p')
+    expect(headers[4].text()).toBe('HGVS.t')
+    expect(headers[5].text()).toBe('Exon/Intron')
+  })
+})
---
+++ src/components/SeqvarConsequencesCard/SeqvarConsequencesCard.stories.ts
@@ -0,0 +1,31 @@
+import { JsonValue } from '@protobuf-ts/runtime'
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import seqvarCsqResultBrca1Json from '../../api/mehari/fixture.seqvarCsqResponse.BRCA1.json'
+import { SeqvarResult } from '../../api/mehari/types'
+import SeqvarConsequencesCard from './SeqvarConsequencesCard.vue'
+
+// Here, fixture data is loaded via `import` from JSON file.  Reading the file
+// as in the tests fails with "process is not defined" error in the browser.
+
+// @ts-ignore
+const seqvarCsqResponseBrca1 = SeqvarResult.fromJson(seqvarCsqResultBrca1Json as JsonValue)
+
+const meta = {
+  title: 'Seqvar/SeqvarConsequencesCard',
+  component: SeqvarConsequencesCard,
+  tags: ['autodocs'],
+  argTypes: {
+    consequences: { control: { type: 'object' } }
+  },
+  args: { consequences: seqvarCsqResponseBrca1.result }
+} satisfies Meta<typeof SeqvarConsequencesCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const BRCA1: Story = {
+  args: {
+    consequences: seqvarCsqResponseBrca1.result
+  }
+}
