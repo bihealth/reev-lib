7d08b2485ed88cd6c237f2ed92043d828f72c99e
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-01-31 08:36:31 +0100

    feat: adding SeqvarVariantValidatorCard (#55)

---
+++ src/components/SeqvarVariantValidatorCard/SeqvarVariantValidatorCard.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import { SeqvarImpl } from '../../lib/genomicVars'
+import SeqvarVariantValidatorCard from './SeqvarVariantValidatorCard.vue'
+
+const seqvarBrca1 = new SeqvarImpl('grch37', '18', 41215920, 'G', 'T')
+
+const meta = {
+  title: 'Seqvar/SeqvarVariantValidatorCard',
+  component: SeqvarVariantValidatorCard,
+  tags: ['autodocs'],
+  argTypes: {
+    seqvar: { control: { type: 'object' } }
+  },
+  args: { seqvar: seqvarBrca1 }
+} satisfies Meta<typeof SeqvarVariantValidatorCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const BRCA1: Story = {
+  args: {
+    seqvar: seqvarBrca1
+  }
+}
