6a9672de6527ad52a0aa9ad0a4323998afb7dfb6
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-02-01 11:58:27 +0100

    fix: use genomeRelease in geneInfo store (#73)

--- src/store/geneInfo/store.ts
+++ src/store/geneInfo/store.ts
@@ -9,6 +9,7 @@ import { ref } from 'vue'
 import { AnnonarsClient } from '../../api/annonars'
 import { DottyClient } from '../../api/dotty'
 import { type HpoTerm, VigunoClient } from '../../api/viguno'
+import { GenomeBuild } from '../../lib/genomeBuilds'
 import { urlConfig } from '../../lib/urlConfig'
 import { StoreState } from '../types'
 
@@ -39,7 +40,7 @@ export const useGeneInfoStore = defineStore('geneInfo', () => {
     transcripts.value = undefined
   }
 
-  const initialize = async (hgncIdQuery: string, genomeRelease: string) => {
+  const initialize = async (hgncIdQuery: string, genomeBuild: GenomeBuild) => {
     // Do not re-load data if the gene symbol is the same
     if (hgncIdQuery === hgncId.value) {
       return
@@ -85,7 +86,7 @@ export const useGeneInfoStore = defineStore('geneInfo', () => {
         const dottyClient = new DottyClient()
         const transcriptsData = await dottyClient.fetchTranscripts(
           hgncIdQuery,
-          genomeRelease === 'grch37' ? 'GRCh37' : 'GRCh38'
+          genomeBuild === 'grch37' ? 'GRCh37' : 'GRCh38'
         )
         transcripts.value = transcriptsData
       }
