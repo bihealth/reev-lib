ca2bd8373c728812c8a9f0f258b7650ffe4c2988
Dzmitry Hramyka <grom.dima.grom@gmail.com>
2024-08-05 12:05:40 +0200

    feat: Increase height in BeaconNetwork card (#240) (#241)

--- src/components/SeqvarBeaconNetworkCard/SeqvarBeaconNetworkCard.vue
+++ src/components/SeqvarBeaconNetworkCard/SeqvarBeaconNetworkCard.vue
@@ -50,7 +50,7 @@ const loadBeacon = () => {
       <iframe
         ref="beaconFrame"
         :src="beaconAddress"
-        style="width: 100%; height: 300px; overflow: auto; border: 0"
+        style="width: 100%; height: 500px; overflow: auto; border: 0"
         vspace="0"
         hspace="0"
       />
